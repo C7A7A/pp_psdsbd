3bb14c3365d20a5a24861a80a5893188a879e888
Mateusz <czajkamateusz999@gmail.com>
2023-03-29 20:54:03 +0200

    MountainEvents pattarns (until -> where, (A->B->(C or D))), match recognize

--- Esper-MountainEvents/src/com/example/bigdata/EsperClient.java
+++ Esper-MountainEvents/src/com/example/bigdata/EsperClient.java
@@ -12,6 +12,7 @@ import net.datafaker.Faker;
 import net.datafaker.fileformats.Format;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Random;
@@ -22,8 +23,8 @@ public class EsperClient {
         int noOfRecordsPerSec;
         int howLongInSec;
         if (args.length < 2) {
-            noOfRecordsPerSec = 30;
-            howLongInSec = 10;
+            noOfRecordsPerSec = 150;
+            howLongInSec = 5;
         } else {
             noOfRecordsPerSec = Integer.parseInt(args[0]);
             howLongInSec = Integer.parseInt(args[1]);
@@ -77,23 +78,78 @@ public class EsperClient {
                 select meff.trip_leader, meff.result, meff.amount_people, me.trip_leader, me.amount_people
                 from MountainEvent(result = "resignation-injury")#length(1) as me, MountainEventFirstFailed as meff
                 where me.amount_people > meff.amount_people and me.trip_leader = meff.trip_leader;"""
+
+        5. Wykrywaj co najmniej 4 kolejne wyprawy, które zakończyły się sukcesem w ciągu 1 sekundy.
+
+        select a[0].ts as ts1, a[1].ts as ts2, a[2].ts as ts3, a[3].ts as ts4,
+        a[0].trip_leader as tl1, a[1].trip_leader as tl2, a[2].trip_leader as tl3, a[3].trip_leader as tl4,
+        a[0].result as r1, a[1].result as r2, a[2].result as r3, a[3].result as r4,
+        b.result as result_end
+        from pattern [
+            every ([4:] a=MountainEvent(result in ("summit-reached", "base-reached"))
+                until b=MountainEvent(result in ("resignation-injury", "resignation-weather", "resignation-someone-missing", "resignation-other"))
+                where timer:within(1 sec)
+            )
+        ]
+
+        6. Wykrywaj przypadki liderów wypraw, których wyprawy zakończyły się trzykrotnie niepowodzeniem lub dwukrotnie niepowodzeniem, a ostatnia wypraw udała się.
+        W przypadku trzykrotnego niepowodzenia, wykrywaj takie wyprawy, że w kazdej kolejnej brało udział WIĘCEJ osób.
+        W przypadku dwukrotnego niepowodzenia, a nastepnie powodzenia wykrywaj wyprawy takie, że:
+        a) w DRUGIEJ wyprawie brało udział WIĘCEJ osób niz w PIERWSZEJ
+        b) w TRZECIEJ wyprawie brało udział MNIEJ osób niż w PIERWSZEJ
+
+        select a.trip_leader as leader,
+        a.result as AResult, b.result as BResult, c.result as CResult, d.result as DResult,
+        a.amount_people, b.amount_people, c.amount_people, d.amount_people
+        from pattern [
+            every (
+                a=MountainEvent(result not in ("summit-reached", "base-reached")) ->
+                b=MountainEvent(b.trip_leader = a.trip_leader and result not in ("summit-reached", "base-reached") and b.amount_people > a.amount_people) -> (
+                    c=MountainEvent(c.trip_leader = a.trip_leader and result not in ("summit-reached", "base-reached") and c.amount_people > b.amount_people) or
+                    d=MountainEvent(d.trip_leader = a.trip_leader and result in ("summit-reached", "base-reached") and d.amount_people < a.amount_people)
+                )
+            )
+        ]
+
+        7. Dla każdego lidera wykrywaj takie wyprawy, że:
+        a) pierwsze wyprawa się nie udała
+        b) od 3-5 kolejnych wypraw udaje się oraz bierze w nich udział mniej osób niż w pierwszej wyprawie
+        c) ostatnie wyprawa nie udaje się oraz bierze w niej udział tyle samo lub więcej osób niż w pierwszej wyprawie
+
+        select * from MountainEvent
+        match_recognize (
+            partition by trip_leader
+            measures
+                A.amount_people as Appl, A.trip_leader as tripLeader,
+                first(B.amount_people) as firstBppl,
+                C.amount_people as Cppl,
+                A.result as Ares, first(B.result) as Bres, C.result as Cres
+            pattern (A B{3, 5} C)
+            define
+                A as A.result not in ("summit-reached", "base-reached"),
+                B as B.amount_people < A.amount_people and B.result in ("summit-reached", "base-reached"),
+                C as C.amount_people >= A.amount_people and C.result not in ("summit-reached", "base-reached")
+        )
+
         */
         try {
             epCompiled = compiler.compile("""
             @public @buseventtype create json schema MountainEvent(peak_name string, trip_leader string, result string, amount_people int, ts string);
-            @name('createWindow')
-                create window MountainEventFirstFailed#length(10) as MountainEvent;
-                
-            @name('fillWindow')
-                insert into MountainEventFirstFailed
-                select *
-                from MountainEvent(result = "resignation-injury")#firstunique(trip_leader);
                 
             @name('result')
-                select meff.trip_leader, meff.result, meff.amount_people, me.trip_leader, me.amount_people
-                from MountainEvent(result = "resignation-injury")#length(1) as me, MountainEventFirstFailed as meff
-                where me.amount_people > meff.amount_people and me.trip_leader = meff.trip_leader;
-                """, compilerArgs);
+            select a.trip_leader as leader,
+                a.result as AResult, b.result as BResult, c.result as CResult, d.result as DResult,
+                a.amount_people, b.amount_people, c.amount_people, d.amount_people
+            from pattern [
+                every (
+                    a=MountainEvent(result not in ("summit-reached", "base-reached")) ->
+                    b=MountainEvent(b.trip_leader = a.trip_leader and result not in ("summit-reached", "base-reached") and b.amount_people > a.amount_people) -> (
+                    c=MountainEvent(c.trip_leader = a.trip_leader and result not in ("summit-reached", "base-reached") and c.amount_people > b.amount_people) or
+                    d=MountainEvent(d.trip_leader = a.trip_leader and result in ("summit-reached", "base-reached") and d.amount_people < a.amount_people)
+                    )
+                )
+            ]
+            """, compilerArgs);
         }
 
         catch (EPCompileException ex) {
@@ -176,7 +232,7 @@ public class EsperClient {
         String record;
         String name = faker.mountain().name();
         String mountaineer = faker.mountaineering().mountaineer();
-        Timestamp timestamp = faker.date().past(30, TimeUnit.SECONDS);
+        Timestamp timestamp = Timestamp.valueOf(LocalDateTime.now());
 
         record = Format.toJson()
                 .set("peak_name", () -> name)
